c73691449733d70bbb81586672b4dd3bb11ee664
alexalextran <alextran8385@gmail.com>
2022-02-03 21:42:19 +1100

    created a random letter generator and an if condition

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,20 +1,70 @@
-import React from 'react';
-
-const Mainpage = () => {
-    document.addEventListener('keypress', (event) => {
-        event.stopImmediatePropagation();
-        var name = event.key;
-        var code = event.code;
-        // Alert the key name and key code on keydown
-        console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
-      }, false);
+import React, { useState } from "react";
 
 
-    return (
-        <div>
-            yooyoyyoyoyo
-        </div>
-    );
-}
+
+const Mainpage = () => { 
+  var keyvalue = ("Start!")
+  var level = 1;
+
+
+
+ 
+
+function makeid() {
+    document.querySelector(".RESULT").innerHTML = "GO!";
+    var possible = "abcdefghijklmnopqrstuvwxyz0123456789`-_=+[{]}|;:',<.>/!@#$%^&*()?";
+
+    for (var i = 0; i < level; i++)
+    keyvalue = (possible.charAt(Math.floor(Math.random() * possible.length)));
+    console.log(keyvalue);
+    
+    document.querySelector(".PRESS").innerHTML = keyvalue;
+    
+    
+    document.addEventListener(
+    "keyup",
+    (event) => {
+      event.stopImmediatePropagation();
+      var name = event.key;
+      var code = event.code;
+     
+      console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
+     
+      if(name == keyvalue){
+        document.querySelector(".RESULT").innerHTML = "NICE!";
+      }
+    },
+    false
+  );
+
+  }
+
+  
+
+  
+
+
+ 
+
+  return (
+    <section>
+      <button onClick={() => setTimeout(makeid, 3000)}></button>
+      <div className="header">
+        <h1>Welcome to Reakt</h1>
+        <p>Where we test how well you know your keyboard!</p>
+      </div>
+      <div className="tutorial">
+        <h1>Tutorial</h1>
+        <p>Before you start, here's a mini guide on how to play Reakt</p>
+        <button> Start </button>
+      </div>
+
+      <div className="timer">3 2 1</div>
+      <div className="PRESS"></div>
+
+      <div className="RESULT"></div>
+    </section>
+  );
+};
 
 export default Mainpage;
