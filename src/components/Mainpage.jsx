400615862b1074998f059193bb2f8896e73c0e25
alexalextran <alextran8385@gmail.com>
2022-02-06 22:17:19 +1100

    added a tutorial button and modal

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import heart from "../assets/heartimage.png";
+import frog from "../assets/frog.png";
 
 const Mainpage = () => {
   var keyvalue = "Start!";
@@ -7,15 +8,16 @@ const Mainpage = () => {
   var streak = 0;
   var life = 1;
   var gameover = false;
-  var avg_time = []
+  var avg_time = [];
+  var toggle_tutorial = true
 
   function makeid(difficulty) {
     if (gameover == true) {
       return;
     }
     document.querySelector(".RESULT").innerHTML = " ";
-    document.querySelector(".heart").style.filter =""
-    document.querySelector(".heart").style.animationName =""
+    document.querySelector(".heart").style.filter = "";
+    document.querySelector(".heart").style.animationName = "";
 
     var possible =
       "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^QWERTYUIOPASDFGHJKLZXCVBNM`~-_=+[{]}|;:',<.>/?";
@@ -26,8 +28,7 @@ const Mainpage = () => {
     document.querySelector(".PRESS").innerHTML = keyvalue;
 
     document.addEventListener("keypress", executegame);
-    start_timer()
-    
+    start_timer();
 
     function executegame(event) {
       event.stopImmediatePropagation();
@@ -36,10 +37,10 @@ const Mainpage = () => {
       document.querySelector(".key").innerHTML = name;
 
       if (name == keyvalue) {
-        avg_time.push(msecVar)
-        console.log(avg_time)
-        stop_timer()
-        
+        avg_time.push(msecVar);
+        console.log(avg_time);
+        stop_timer();
+
         streak++;
         keyvalue = "";
         document.querySelector(".current__streak--value").innerHTML = streak;
@@ -52,19 +53,18 @@ const Mainpage = () => {
           makeid(difficulty);
         }, 4000);
       } else {
-
         life--;
-        document.querySelector(".heart").style.filter ="grayscale(1)"
-        document.querySelector(".heart").style.animationName ="shake"
+        document.querySelector(".heart").style.filter = "grayscale(1)";
+        document.querySelector(".heart").style.animationName = "shake";
         if (life < 0) {
-          stop_timer()
+          stop_timer();
           const sum = avg_time.reduce((a, b) => a + b, 0);
-          var average = (sum / avg_time.length).toFixed(0) || 0
-          
-          document.querySelector(".gameover").style.animation = "fade 800ms"
-          document.querySelector(".gameover").style.display ="flex"
-          document.getElementById("average").innerHTML = average
-          document.getElementById("streak").innerHTML = streak
+          var average = (sum / avg_time.length).toFixed(0) || 0;
+
+          document.querySelector(".gameover").style.animation = "fade 800ms";
+          document.querySelector(".gameover").style.display = "flex";
+          document.getElementById("average").innerHTML = average;
+          document.getElementById("streak").innerHTML = streak;
 
           gameover = true;
           document.removeEventListener("keypress", executegame);
@@ -78,38 +78,32 @@ const Mainpage = () => {
       "timer 4s linear";
   }
 
-     
-      var timerOn = false
-      var msecVar = 0
-      var x
-      
-        function setMsec() {
-        
-          msecVar = msecVar + 5;
-         x = setTimeout(setMsec, 1);
-          document.getElementById("msec").innerHTML = `<p>${msecVar}</p>`
-      }
-      
-      function start_timer() {
-   
-        if (!timerOn) {
-          timerOn = true;
-          setMsec();
-        }
-      }
-      
-      function stop_timer() {
-        
-          clearTimeout(x);  
-          reset_timer()
-          timerOn = false;
-        }
+  var timerOn = false;
+  var msecVar = 0;
+  var x;
 
-        function reset_timer() {
-         msecVar = 0
-      }
-      
+  function setMsec() {
+    msecVar = msecVar + 5;
+    x = setTimeout(setMsec, 1);
+    document.getElementById("msec").innerHTML = `<p>${msecVar}</p>`;
+  }
 
+  function start_timer() {
+    if (!timerOn) {
+      timerOn = true;
+      setMsec();
+    }
+  }
+
+  function stop_timer() {
+    clearTimeout(x);
+    reset_timer();
+    timerOn = false;
+  }
+
+  function reset_timer() {
+    msecVar = 0;
+  }
 
   function setdifficulty(mode) {
     if (mode == "easy") {
@@ -134,16 +128,25 @@ const Mainpage = () => {
     }
   }
 
+  function toggle__tutorial() {
+    if(!toggle_tutorial){
+      document.querySelector(".tutorial").style.opacity = "0"
+      document.querySelector(".tutorial").style.display = "none"
+    }
+    else{
+      document.querySelector(".tutorial").style.opacity = "1"
+      document.querySelector(".tutorial").style.display = "flex"
+    }
+    toggle_tutorial = !toggle_tutorial
+  }
+
+  
+
   return (
     <section>
       <div className="header">
         <h1>Welcome to Reakt</h1>
         <p>Where we test how well you know your keyboard!</p>
-      </div>
-      <div className="tutorial">
-        <h1>Tutorial</h1>
-        <p>Before you start, here's a mini guide on how to play Reakt</p>
-        <button> Start </button>
       </div>
 
       <div className="difficulty">
@@ -194,20 +197,43 @@ const Mainpage = () => {
       </div>
 
       <div className="stopwatch">
-      <div id="msec">
-            0
-        </div>
+        <div id="msec">0</div>
       </div>
 
       <div className="gameover">
-            <div className="gameover__card">
-              <h2>Game Over</h2>
-              <p className="gameover__card--text">Nice try you got a streak of <span className="red" id="streak">14</span> and you averaged <span className="red" id="average">700ms</span></p>
-              <button>Wanna try again?</button>
-            </div>
+        <div className="gameover__card">
+          <h2>Game Over</h2>
+          <p className="gameover__card--text">
+            Nice try you got a streak of{" "}
+            <span className="red" id="streak">
+              14
+            </span>{" "}
+            and you averaged{" "}
+            <span className="red" id="average">
+              700ms
+            </span>
+          </p>
+          <button>Wanna try again?</button>
+        </div>
       </div>
 
+      <img className="tutorial__frog" onClick={() => toggle__tutorial()} src={frog}></img>
+      <p className="tutorial__text">Wanna know how to play? Click on me!</p>
 
+      <div className="tutorial"  onClick={() => toggle__tutorial()}>
+        <div className="tutorial__card">
+          <p>
+            The moment you click a difficulty, you'll be given 3 seconds to
+            prepare, once the countdown ends, press the respective key on your
+            keyboard that matches the key displayed on screen
+          </p>
+          <p>
+            Also for each round, you have one life, incase you accidently fat
+            finger a key, and NOTE SOME KEYS REQUIRE YOU TO PRESS SHIFT/CAPSLOCK
+            FIRST I.E CAPITAL LETTERS
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
